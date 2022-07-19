bb120efa6ab29fe8a39702050dcc02a2b48d42cc
gomez-fernando <ferdgomez@hotmail.es>
2022-07-19 13:26:47 +0200

    Fix issue with login failed

--- src/pages/login/login-page.tsx
+++ src/pages/login/login-page.tsx
@@ -7,8 +7,8 @@ import { loadRoomsAction } from '../../reducers/room/action.creators';
 import { loadUsersAction } from '../../reducers/user/action.creators';
 import { ApiChat } from '../../services/api';
 import { LocalStoreService } from '../../services/local-storage';
-import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
-import { storage } from "../../firebase";
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import { storage } from '../../firebase';
 import styles from './index.module.css';
 import { iUser } from '../../interfaces/interfaces';
 import { socket } from '../../chat/chat-socket';
@@ -23,7 +23,13 @@ export default function LoginPage() {
 
     const navigate = useNavigate();
     const initialState = { email: '', password: '' };
-    const initSignUp = { name: '', nickname: '',  email: '', avatar: '', password: '' };
+    const initSignUp = {
+        name: '',
+        nickname: '',
+        email: '',
+        avatar: '',
+        password: '',
+    };
     const [formData, setFormData] = useState(initialState);
     const [signUp, setSignUp] = useState(initSignUp);
 
@@ -44,29 +50,41 @@ export default function LoginPage() {
         setLoading(true);
 
         const resp = await apiChat.login(formData);
-        const rooms = await apiChat.getAllRoomsByUser(
-            resp.user._id,
-            resp.token
-        );
-        const users = await apiChat.getAllUsers(resp.user._id, resp.token);
+
+        console.log(resp);
+
+        if (resp.status === 201) {
+            const rooms = await apiChat.getAllRoomsByUser(
+                resp.user._id,
+                resp.token
+            );
+            const users = await apiChat.getAllUsers(resp.user._id, resp.token);
             setLoading(false);
 
-        let user = resp.user;
-        user = { ...user, token: resp.token };
+            let user = resp.user;
+            user = { ...user, token: resp.token };
 
-        dispatcher(loadLoggedUsersAction([user]));
-        dispatcher(loadUsersAction(users));
-        dispatcher(loadRoomsAction(rooms));
+            dispatcher(loadLoggedUsersAction([user]));
+            dispatcher(loadUsersAction(users));
+            dispatcher(loadRoomsAction(rooms));
 
-        localStorage.setUser(user._id);
-        localStorage.setToken(user.token);
-        const newUser: iUser = {
-            ...user, online: true, token: user.token
+            localStorage.setUser(user._id);
+            localStorage.setToken(user.token);
+            const newUser: iUser = {
+                ...user,
+                online: true,
+                token: user.token,
+            };
+
+            socket.emit('update-user', newUser);
+
+            navigate(`/`);
+        } else {
+            // TODO improve this
+            alert('Se ha producido un error en login');
+            setLoading(false);
+            navigate('/');
         }
-
-        socket.emit('update-user', newUser);
-
-        navigate(`/`);
     };
 
     async function handleUpload(ev: SyntheticEvent) {
@@ -79,127 +97,197 @@ export default function LoginPage() {
             file as unknown as Blob | Uint8Array | ArrayBuffer
         );
         const url = await getDownloadURL(ref(storage, `/files/${file.name}`));
-        setSignUp({ ...signUp, avatar: url })
+        setSignUp({ ...signUp, avatar: url });
     }
 
     const handleSubmitSignUp = async (ev: SyntheticEvent) => {
         ev.preventDefault();
         const resp = await apiChat.signup(signUp);
+        console.log(resp);
 
-        if(resp.status === 201){
+        if (resp.status === 201) {
             closeModal();
-        } else{
+        } else {
             // TODO improve this
-            alert('Se ha producido un error')
+            alert('Se ha producido un error');
         }
     };
 
     const openModal = () => {
         document.querySelector('#loginform')?.classList.add(`${styles.d_none}`);
-        document.querySelector('#signupform')?.classList.remove(`${styles.d_none}`);
-    }
+        document
+            .querySelector('#signupform')
+            ?.classList.remove(`${styles.d_none}`);
+    };
 
     const closeModal = () => {
-        document.querySelector('#loginform')?.classList.remove(`${styles.d_none}`);
-        document.querySelector('#signupform')?.classList.add(`${styles.d_none}`);
-    }
+        document
+            .querySelector('#loginform')
+            ?.classList.remove(`${styles.d_none}`);
+        document
+            .querySelector('#signupform')
+            ?.classList.add(`${styles.d_none}`);
+    };
 
     return (
         <>
             {loading ? (
-                <Spinner  />
+                <Spinner />
             ) : (
-                <div  className={styles.container}>
-                <div className={styles.logo_section}>
-                    <img src="./assets/logo.png" alt="" />
-                </div>
-                <div>
-                <form id='loginform' onSubmit={handleSubmit} className={styles.form}>
-                    <div>
-                        <div><label htmlFor="">Email</label></div>
-                        <div><input type="email" name="email" onChange={handleChange} required /></div>
+                <div className={styles.container}>
+                    <div className={styles.logo_section}>
+                        <img src="./assets/logo.png" alt="" />
                     </div>
                     <div>
-                        <div><label htmlFor="">Password</label></div>
-                        <div>
-                            <input
-                                type="password"
-                                name="password"
-                                onChange={handleChange}
-                                required
-                            />
-                        </div>
+                        <form
+                            id="loginform"
+                            onSubmit={handleSubmit}
+                            className={styles.form}
+                        >
+                            <div>
+                                <div>
+                                    <label htmlFor="">Email</label>
+                                </div>
+                                <div>
+                                    <input
+                                        type="email"
+                                        name="email"
+                                        onChange={handleChange}
+                                        required
+                                    />
+                                </div>
+                            </div>
+                            <div>
+                                <div>
+                                    <label htmlFor="">Password</label>
+                                </div>
+                                <div>
+                                    <input
+                                        type="password"
+                                        name="password"
+                                        onChange={handleChange}
+                                        required
+                                    />
+                                </div>
+                            </div>
+                            <div>
+                                <div>
+                                    <button type="submit">Login</button>
+                                </div>
+                            </div>
+                            <div>
+                                <div>
+                                    <p>¿No tienes cuenta?</p>
+                                </div>
+                                <div>
+                                    <button onClick={openModal}>
+                                        Regístrate
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
-                    <div>
-                        <div><button type="submit">Login</button></div>
-                    </div>
-                    <div>
-                        <div><p>¿No tienes cuenta?</p></div>
-                        <div>
-                            <button onClick={openModal} >Regístrate</button>
-                        </div>
-                    </div>
-                </form>
-                </div>
-    
-    
-                <div>
-                <form id='signupform' onSubmit={handleSubmitSignUp} className={ `${styles.modal} ${styles.d_none} ${styles.form}`}>
-                    <div>
-                        <div><label htmlFor="">Nombre</label></div>
-                        <div><input type="text" name="name" onChange={handleChangeSignUp} required /></div>
-                    </div>
-                    <div>
-                        <div><label htmlFor="">Apellido</label></div>
-                        <div><input type="text" name="surname" onChange={handleChangeSignUp} required /></div>
-                    </div>
-                    <div>
-                        <div><label htmlFor="">Nick name</label></div>
-                        <div><input type="text" name="nickname" onChange={handleChangeSignUp} required /></div>
-                    </div>
-                    <div>
-                        <div><label htmlFor="">Email</label></div>
-                        <div><input type="email" name="email" onChange={handleChangeSignUp} required /></div>
-                    </div>
-                    <div>
-                        <div><label htmlFor="">Password</label></div>
-                        <div>
-                            <input
-                                type="password"
-                                name="password"
-                                onChange={handleChangeSignUp}
-                                required
-                            />
-                        </div>
-                    </div>
-                    <div>
-                        <div><label htmlFor="">Avatar</label></div>
-                        <div>
-                            <input
-                                type="file"
-                                name="avatar"
-                                onChange={handleUpload}
-                            />
-                        </div>
-                    </div>
-                    <div>
-                        <div><button type="submit">Registrarme</button></div>
-                    </div>
-                    <div>
-                        <div><p>¿Ya tienes cuenta?</p></div>
-                        <div>
-                            <button onClick={closeModal} >Login</button>
-                        </div>
-                    </div>
-                </form>
-                </div>
-            </div>
-            )
 
-            }
+                    <div>
+                        <form
+                            id="signupform"
+                            onSubmit={handleSubmitSignUp}
+                            className={`${styles.modal} ${styles.d_none} ${styles.form}`}
+                        >
+                            <div>
+                                <div>
+                                    <label htmlFor="">Nombre</label>
+                                </div>
+                                <div>
+                                    <input
+                                        type="text"
+                                        name="name"
+                                        onChange={handleChangeSignUp}
+                                        required
+                                    />
+                                </div>
+                            </div>
+                            <div>
+                                <div>
+                                    <label htmlFor="">Apellido</label>
+                                </div>
+                                <div>
+                                    <input
+                                        type="text"
+                                        name="surname"
+                                        onChange={handleChangeSignUp}
+                                        required
+                                    />
+                                </div>
+                            </div>
+                            <div>
+                                <div>
+                                    <label htmlFor="">Nick name</label>
+                                </div>
+                                <div>
+                                    <input
+                                        type="text"
+                                        name="nickname"
+                                        onChange={handleChangeSignUp}
+                                        required
+                                    />
+                                </div>
+                            </div>
+                            <div>
+                                <div>
+                                    <label htmlFor="">Email</label>
+                                </div>
+                                <div>
+                                    <input
+                                        type="email"
+                                        name="email"
+                                        onChange={handleChangeSignUp}
+                                        required
+                                    />
+                                </div>
+                            </div>
+                            <div>
+                                <div>
+                                    <label htmlFor="">Password</label>
+                                </div>
+                                <div>
+                                    <input
+                                        type="password"
+                                        name="password"
+                                        onChange={handleChangeSignUp}
+                                        required
+                                    />
+                                </div>
+                            </div>
+                            <div>
+                                <div>
+                                    <label htmlFor="">Avatar</label>
+                                </div>
+                                <div>
+                                    <input
+                                        type="file"
+                                        name="avatar"
+                                        onChange={handleUpload}
+                                    />
+                                </div>
+                            </div>
+                            <div>
+                                <div>
+                                    <button type="submit">Registrarme</button>
+                                </div>
+                            </div>
+                            <div>
+                                <div>
+                                    <p>¿Ya tienes cuenta?</p>
+                                </div>
+                                <div>
+                                    <button onClick={closeModal}>Login</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            )}
         </>
-        
-
-        
     );
 }
